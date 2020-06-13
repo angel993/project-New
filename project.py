bb703e6d0fe92f72dd1728e795a3acc9ec03b960
angel993 <angelsilvestrerojasblanca@gmail.com>
2020-06-13 19:11:06 -0300

    correcion en proyect.py

--- project.py
+++ project.py
@@ -1,11 +1,11 @@
 nota1 = 3
 nota2 = 7.5
 nota3 = 5.3
-nota4 = 8
+nota4 = 9
 nota_total = (nota1 + nota2 + nota3 + nota4) / 4  
 
 if nota_total <= 6:
-    print('Usted no aprobo el año escolar')
+    print('Usted no aprobo el año escolar. Su nota fue:' + str(nota_total))
 else:
     print('Usted aprobo el año escolar su nota fue: ' + str(nota_total))
 
